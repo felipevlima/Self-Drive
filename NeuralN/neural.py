3c2134c9fc2b6491f1df0bbb5b0e45002966f5c2
Felipe Vieira <felip.3lima@hotmail.com>
2018-12-17 18:10:29 -0300

    Update neural.py

--- NeuralN/neural.py
+++ NeuralN/neural.py
@@ -1,15 +1,40 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
+def draw(x1, x2):
+    ln = plt.plot(x1, x2)
+
+def sigmoid(score):
+    return 1/(1 + np.exp(-score))
+
 n_pts = 100
-#random_x1_values = np.random.normal(10, 2, n_pts)
-#random_x2_values = np.random.normal(12, 2, n_pts)
+np.random.seed(0)
+bias = np.ones(n_pts)
+
 top_region = np.array([np.random.normal(10, 2, n_pts),
-                       np.random.normal(12, 2, n_pts)]).T
+                       np.random.normal(12, 2, n_pts),
+                       bias]).T
 bottom_region = np.array([np.random.normal(5, 2, n_pts),
-                          np.random.normal(6, 2, n_pts)]).T
+                          np.random.normal(6, 2, n_pts),
+                          bias]).T
+all_points = np.vstack((top_region, bottom_region))
+
+w1 = -0.2
+w2 = -0.35
+bias = 3.5
+
+line_parameters = np.matrix([w1, w2, bias]).T
+x1 = np.array([bottom_region[:, 0].min(), top_region[:, 0].max()])
+x2 = - bias / w2 + x1 * (-w1 / w2)
+
+linear_combination = all_points*line_parameters
+probabilities = sigmoid(linear_combination)
+print(probabilities)
+
 _, ax = plt.subplots(figsize=(4, 4))
 ax.scatter(top_region[:, 0], top_region[:, 1], color='r')
 ax.scatter(bottom_region[:, 0], bottom_region[:, 1], color='b')
+draw(x1,x2)
 plt.show()
 print(top_region)
