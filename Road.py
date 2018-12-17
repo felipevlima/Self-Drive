c7e250e02d999005b89bf8b74b0b2d05cf3f7008
Felipe Vieira <31170444+felipevlima@users.noreply.github.com>
2018-12-17 12:27:22 -0300

    Update Road.py

--- Road.py
+++ Road.py
@@ -52,7 +52,7 @@ def region_of_interest(img):
     masked_img = cv.bitwise_and(img,  mask)
     return masked_img
     
-img = cv.imread("**YOUR IMAGE ANDRESS**")
+img = cv.imread("teste.jpg")
 lane_image = np.copy(img)
 canny_img = canny(lane_image)
 cropped_img = region_of_interest(canny_img)
