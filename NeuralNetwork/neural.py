7eb60dc04b449541cf9359f151d7a0ac778e88e8
Felipe Vieira <felip.3lima@hotmail.com>
2018-12-17 21:18:48 -0300

    Update neural.py

--- NeuralNetwork/neural.py
+++ NeuralNetwork/neural.py
@@ -8,33 +8,39 @@ def draw(x1, x2):
 def sigmoid(score):
     return 1/(1 + np.exp(-score))
 
-n_pts = 100
+def calculate_error(line_parameters, points, y):
+    m = points.shape[0]
+    p = sigmoid(points*line_parameters)
+    cross_entropy = -(1/m)*(np.log(p).T * y + np.log(1-p).T*(1-y))
+    return cross_entropy
+
+n_pts = 10
 np.random.seed(0)
 bias = np.ones(n_pts)
-
 top_region = np.array([np.random.normal(10, 2, n_pts),
                        np.random.normal(12, 2, n_pts),
                        bias]).T
 bottom_region = np.array([np.random.normal(5, 2, n_pts),
                           np.random.normal(6, 2, n_pts),
                           bias]).T
+
 all_points = np.vstack((top_region, bottom_region))
-
-w1 = -0.2
-w2 = -0.35
-bias = 3.5
-
+w1 = -0.1
+w2 = -0.15
+bias = 0
 line_parameters = np.matrix([w1, w2, bias]).T
 x1 = np.array([bottom_region[:, 0].min(), top_region[:, 0].max()])
 x2 = - bias / w2 + x1 * (-w1 / w2)
 
 linear_combination = all_points*line_parameters
-probabilities = sigmoid(linear_combination)
-print(probabilities)
+
+y =  np.array([np.zeros(n_pts), np.ones(n_pts)]).reshape(n_pts*2, 1)
+
 
 _, ax = plt.subplots(figsize=(4, 4))
 ax.scatter(top_region[:, 0], top_region[:, 1], color='r')
 ax.scatter(bottom_region[:, 0], bottom_region[:, 1], color='b')
 draw(x1,x2)
 plt.show()
-print(top_region)
+
+print((calculate_error(line_parameters, all_points, y)))
