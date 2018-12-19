856ace5d61719b07f54928292bdcd8422cd06935
Felipe Vieira <felip.3lima@hotmail.com>
2018-12-19 17:25:25 -0300

    Update keras.py
    Add more functions to trainning Tensroflow.Keras

--- NeuralNetwork/keras.py
+++ NeuralNetwork/keras.py
@@ -1,7 +1,13 @@
 import numpy as np
 import keras
 import matplotlib.pyplot as plt
-from keras.models import Sequential
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.optimizers import Adam
+
+def plot_decision_boundary(X, y, model):
+    x_span = np.linspace(min(X[:, 0]), max(X[:, 0]))
+    print(x_span)
 
 n_pts = 500
 np.random.seed(0)
@@ -15,4 +21,23 @@ y = np.matrix(np.append(np.zeros(n_pts), np.ones(n_pts))).T
  
 plt.scatter(X[:n_pts,0], X[:n_pts,1])
 plt.scatter(X[n_pts:,0], X[n_pts:,1])
-plt.show()
+
+model = Sequential()
+model.add(Dense(units= 1, input_shape=(2,), activation='sigmoid'))
+adam = Adam(lr = 0.1)
+model.compile(adam, loss='binary_crossentropy', metrics=['accuracy'])
+h = model.fit(x=X, y=y, verbose=1, batch_size=50, epochs = 1, shuffle='true')
+
+plot_decision_boundary(X, y, model)
+
+plt.plot(h.history['acc'])
+plt.title('accuracy')
+plt.xlabel('epoch')
+plt.legend(['accurancy'])
+
+
+plt.plot(h.history['loss'])
+plt.title('loss')
+plt.xlabel('epoch')
+plt.legend(['loss'])
+
