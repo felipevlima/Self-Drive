49317029bfe86790ddd3f8450daf8b35e9594cc6
Felipe Vieira <felip.3lima@hotmail.com>
2018-12-19 17:49:56 -0300

    Update keras.py

--- NeuralNetwork/keras.py
+++ NeuralNetwork/keras.py
@@ -22,6 +22,7 @@ y = np.matrix(np.append(np.zeros(n_pts), np.ones(n_pts))).T
 plt.scatter(X[:n_pts,0], X[:n_pts,1])
 plt.scatter(X[n_pts:,0], X[n_pts:,1])
 
+
 model = Sequential()
 model.add(Dense(units= 1, input_shape=(2,), activation='sigmoid'))
 adam = Adam(lr = 0.1)
@@ -29,6 +30,7 @@ model.compile(adam, loss='binary_crossentropy', metrics=['accuracy'])
 h = model.fit(x=X, y=y, verbose=1, batch_size=50, epochs = 1, shuffle='true')
 
 plot_decision_boundary(X, y, model)
+
 
 plt.plot(h.history['acc'])
 plt.title('accuracy')
