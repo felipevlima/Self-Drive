7b78a6387c4f97f6b866502749a44d0babd35397
Felipe Vieira <felip.3lima@hotmail.com>
2018-12-19 20:14:00 -0300

    Update keras.py

--- NeuralNetwork/keras.py
+++ NeuralNetwork/keras.py
@@ -6,9 +6,16 @@ from tensorflow.keras.layers import Dense
 from tensorflow.keras.optimizers import Adam
 
 def plot_decision_boundary(X, y, model):
-    x_span = np.linspace(min(X[:, 0]), max(X[:, 0]))
-    print(x_span)
-
+    x_span = np.linspace(min(X[:, 0]) - 1, max(X[:, 0]) + 1)
+    y_span = np.linspace(min(X[:, 1]) - 1, max(X[:, 1]) + 1)
+    xx, yy = np.meshgrid(x_span, y_span)
+    xx_, yy_ = xx.ravel(), yy.ravel()
+    grid = np.c_[xx_, yy_]
+    pred_func = model.predict(grid)
+    z = pred_func.reshape(xx.shape)
+    teste = plt.contourf(xx, yy, z)
+    plt.show(teste)
+    
 n_pts = 500
 np.random.seed(0)
 Xa = np.array([np.random.normal(13, 2, n_pts),
@@ -30,16 +37,23 @@ model.compile(adam, loss='binary_crossentropy', metrics=['accuracy'])
 h = model.fit(x=X, y=y, verbose=1, batch_size=50, epochs = 1, shuffle='true')
 
 plot_decision_boundary(X, y, model)
+plt.scatter(X[:n_pts,0], X[:n_pts,1])
+plt.scatter(X[n_pts:,0], X[n_pts:,1])
+x = 7.5
+y = 5
+point = np.array([[x, y]])
+prediction = model.predict(point)
+plt.plot([x], [y], marker='o', markersize=10, color='red')
+print("Prediction is: ", prediction)
+
+#plt.plot(h.history['acc'])
+#plt.title('accuracy')
+#plt.xlabel('epoch')
+#plt.legend(['accurancy'])
 
 
-plt.plot(h.history['acc'])
-plt.title('accuracy')
-plt.xlabel('epoch')
-plt.legend(['accurancy'])
-
-
-plt.plot(h.history['loss'])
-plt.title('loss')
-plt.xlabel('epoch')
-plt.legend(['loss'])
+#plt.plot(h.history['loss'])
+#plt.title('loss')
+#plt.xlabel('epoch')
+#plt.legend(['loss'])
 
